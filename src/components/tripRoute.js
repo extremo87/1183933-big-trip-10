d902a20ed74273ff0e583127b1022894b1a34e97
Sergey Ukhanov <ukhanov.sergey@gmail.com>
2020-01-26 15:46:01 +0600

    B31

--- src/components/tripRoute.js
+++ src/components/tripRoute.js
@@ -1,6 +1,9 @@
 import Component from './component';
 import moment from 'moment';
 
+const MAX_CITIES_COUNT = 3;
+const MIDDLE_CITIES_COUNT = 2;
+
 export default class TripRoute extends Component {
 
   constructor(points) {
@@ -27,11 +30,11 @@ export default class TripRoute extends Component {
     this._points.map((point) => cities.add(point.city.name));
 
     let title;
-    if (cities.size > 3) {
+    if (cities.size > MAX_CITIES_COUNT) {
       title = `${startPoint.city.name} &mdash; ... &mdash; ${finalPoint.city.name}`;
-    } else if (cities.size === 3) {
+    } else if (cities.size === MAX_CITIES_COUNT) {
       title = `${startPoint.city.name} &mdash; ${this._points[1].city.name} &mdash; ${finalPoint.city.name}`;
-    } else if (cities.size === 2) {
+    } else if (cities.size === MIDDLE_CITIES_COUNT) {
       title = `${startPoint.city.name} &mdash; ${finalPoint.city.name}`;
     } else {
       title = `${startPoint.city.name} &mdash; ${startPoint.city.name}`;
